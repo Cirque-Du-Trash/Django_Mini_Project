85f6badbe296932b32b5518fa5371fe78353ae36
mr-cliche-git <mr.cliche7@gmail.com>
2024-10-26 03:22:46 +0900

    build : Django Basic Settings

---
+++ config/settings/base.py
@@ -0,0 +1,14 @@
+INSTALLED_APPS = [
+    'rest_framework',
+    'drf_spectacular',
+]
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Your API',
+    'DESCRIPTION': 'Your project description',
+    'VERSION': '1.0.0',
+}
