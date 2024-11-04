d082f366f14cf77efd5efbdc108629159e7c7585
BE_05_이동혁 <ilwdsx1@naver.com>
2024-11-04 18:47:24 +0900

    "✨ feat : 유저 로그인 로그아웃 구현기능 추가 "

---
+++ users/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+User = get_user_model()
+
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('email', 'password')
+
+    def create(self, validated_data):
+        user = User(**validated_data)
+        user.set_password(validated_data['password'])  # 비밀번호 해싱
+        user.save()
+        return user
+
+
+class UserLoginSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        token['email'] = user.email
+        return token
+
+
+class LogoutSerializer(serializers.Serializer):
+    token = serializers.CharField()
